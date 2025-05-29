31649db09b6712ecedf5a697a87bdb91f020a47d
Shabna12 <shabnababu9@gmail.com>
2025-05-29 19:43:42 +0530

    processing

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator} from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName', 
+    'email', 
+    'dob', 
+    'gender', 
+    'education', 
+    'company', 
+    'experience', 
+    'package',
+    'action',
+  ];
+  dataSource = new MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort
+
+  constructor (private _dialog: MatDialog, private _empService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+
+  openAddEditEmpForm(){
+    this._dialog.open(EmpAddEditComponent)
+  }
+
+  getEmployeeList(){
+    this._empService.getEmployeeList().subscribe({
+      next:(res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  // applying filter
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if(this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+}
